925daf1cb258077e8702d332cb1caa6366976319
bmugle <mugle.ba68@gmail.com>
2020-12-30 00:24:01 +0530

    adding line

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express();
 const port = 8080;
 
 app.get('/', (req, res) => res.send('Hello Bhausaheb!'));
+app.get('/', (req, res) => res.send('Hello Word!'));
+
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
